6e770f9cdd2919df97dc49b22b04f443fb7261cf
petros05 <abrahapetros0@gmail.com>
2026-02-16 11:53:41 -0700

    Add 3.5 Exercises AND fixed input01.java file

---
+++ 3-5 Exercises/src/Input01.java
@@ -0,0 +1,24 @@
+import javax.swing.JOptionPane;
+
+public class Input01 {
+    public static void main(String[] args) {
+        //Create a JOptionPane.
+        //Store the input as a String and print it.
+        String input = JOptionPane.showInputDialog("Type something: ");
+        System.out.println(input);
+        
+        
+        //Parse the input as an int.
+        //Print its value +1
+        int num = Integer.parseInt(input);
+        System.out.println(num + 1);
+        
+        
+        
+        //Try creating a dialog, parsing it, and initializing an int in a single line.
+        //You should have only one semicolon (;) in this line.
+        System.out.println(Integer.parseInt(JOptionPane.showInputDialog("Input something: ")));
+
+        
+    }
+}
