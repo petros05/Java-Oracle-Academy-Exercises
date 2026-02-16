16963973e0e938dee2d1a20e3dbce7b965104e38
petros05 <abrahapetros0@gmail.com>
2026-02-16 12:17:34 -0700

    Fixed Input03.java

--- 3-5 Exercises/src/Input03.java
+++ 3-5 Exercises/src/Input03.java
@@ -1,14 +1,22 @@
+import java.util.Scanner;
+
 class Input03 {
 
     public static void main(String[] args) {
         //Create a Scanner
-        
+        Scanner scan = new Scanner(System.in);
+
         //Find and print the sum of three integers entered by the user
-        
-        
+        System.out.println("Enter a number: ");
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+        int num3 = scan.nextInt();
+
+        // Print sum of three integers entered by user
+        System.out.println("Sum of numbers entered: " + (num1 + num2 + num3));
         
         
         //Remember to close the Scanner
-        
+        scan.close();
     }
 }
